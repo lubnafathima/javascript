0cdc0c6e3a4366148b8c910b7266095627428cce
Lubna Fathima <lubnafathima275@gmail.com>
2024-07-08 13:19:31 +0530

    init: added init code

---
+++ script.js
@@ -0,0 +1,65 @@
+console.log("Start");
+
+// function a() {
+//     console.log("A");
+// }
+// a();
+
+// ________________________________________________________________
+
+// setTimeout(function cb() {
+//   console.log("Callback");
+// }, 1000);
+
+// ________________________________________________________________
+
+// document.getElementById("btn").addEventListener("click", function cb() {
+//     console.log("callback");
+// })
+
+// ________________________________________________________________
+
+// setTimeout(function cb() {
+//   console.log("Callback");
+// }, 1000);
+
+// fetch("https://jsonplaceholder.typicode.com/todos/1").then(function cbF() {
+//     console.log("Fetched");
+// })
+
+// ________________________________________________________________
+
+const cart = ["milk", "bread", "butter"];
+
+// createOrder(cart, function () {
+//     payment();
+// });
+
+// ________________________________________________________________
+
+// createOrder(cart, function () {
+//     payment(function() {
+//         summary(function() {
+//             wallet();
+//         })
+//     })
+// })
+
+// ________________________________________________________________
+
+// const promise = createOrder(cart);
+
+// promise.then(function (orderId) {
+//   return payment(orderId);
+// });
+
+// ________________________________________________________________
+
+// createOrder(cart)
+//   .then(() => payment())
+//   .then(() => summary())
+//   .then(() => wallet());
+
+// ________________________________________________________________
+
+console.log("End");
