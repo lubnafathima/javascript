e569ac7090e6c906740082544966b91c5327ddd2
Lubna Fathima <lubnafathima275@gmail.com>
2024-07-18 16:27:37 +0530

    feat: Promise.all() - all case success

---
+++ promiseAPIs.js
@@ -0,0 +1,20 @@
+// Promise.all() - when all cases are success
+const p1 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("P1 Success"), 3000);
+});
+
+const p2 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("P2 Success"), 1000);
+});
+
+const p3 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve("P3 Success"), 2000);
+});
+
+Promise.all([p1, p2, p3])
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
